2cf309896f777801d207b990dbdf30e5bdb7a798
cobala9211 <dnphuong9211@gmail.com>
2016-08-10 11:34:04 +0700

    add all project

---
+++ app/src/main/java/com/example/dnp/videoapp/activity/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.dnp.videoapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dnp.videoapp.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+}
