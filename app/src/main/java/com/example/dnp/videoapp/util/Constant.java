f88253cd580129aa8ba6f624ed8722a0152ac33b
cobala9211 <dnphuong9211@gmail.com>
2016-08-10 16:47:59 +0700

    add new file constant

---
+++ app/src/main/java/com/example/dnp/videoapp/util/Constant.java
@@ -0,0 +1,16 @@
+package com.example.dnp.videoapp.util;
+
+/**
+ * Created by dnp on 10/08/2016.
+ */
+public class Constant {
+
+    public static final int COUNT_TYPE_LIST = 3;
+
+    public static final int TYPE_LIST_USER = 0;
+
+    public static final int TYPE_LIST_CONTEXT = 1;
+
+    public static final int TYPE_LIST_SETTING = 2;
+
+}
