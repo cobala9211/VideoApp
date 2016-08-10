e4fff6c8b32f48f3d832574ff9859891e6d61f84
cobala9211 <dnphuong9211@gmail.com>
2016-08-10 16:48:46 +0700

    add file NavigatationItem

---
+++ app/src/main/java/com/example/dnp/videoapp/model/NavigatationItem.java
@@ -0,0 +1,36 @@
+package com.example.dnp.videoapp.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by dnp on 10/08/2016.
+ */
+
+@RequiredArgsConstructor(suppressConstructorProperties = true, access = AccessLevel.PUBLIC)
+@AllArgsConstructor(suppressConstructorProperties = true, access = AccessLevel.PUBLIC)
+@Data
+public class NavigatationItem {
+
+    /**
+     * url image profile user
+     **/
+    private int url;
+
+    /**
+     * title user
+     **/
+    private String title;
+
+    /**
+     * count video
+     **/
+    private int count;
+
+    /**
+     * type item listview
+     **/
+    private int type;
+}
