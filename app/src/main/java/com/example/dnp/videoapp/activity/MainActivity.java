65a2f0c8c21de6c164a84ab1f33726a9924456cd
cobala9211 <dnphuong9211@gmail.com>
2016-08-16 21:01:03 +0700

    create funtion upload file, config gradle firebase, create object VideoOnline

--- app/src/main/java/com/example/dnp/videoapp/activity/MainActivity.java
+++ app/src/main/java/com/example/dnp/videoapp/activity/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.dnp.videoapp.activity;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,20 +23,41 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.example.dnp.videoapp.R;
 import com.example.dnp.videoapp.adapter.NavigateAdapter;
 import com.example.dnp.videoapp.model.RowItem;
 import com.example.dnp.videoapp.model.Users;
+import com.example.dnp.videoapp.model.VideoOnline;
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
 
 /**
  * Created by dnp on 10/08/2016.
  */
 @EActivity(R.layout.activity_main)
 public class MainActivity extends BaseActivity {
+
+    public static final int FILE_SELECT_CODE = 0;
+    private List<RowItem> mListRowItem = new ArrayList<>();
+    private ProgressDialog mProgressDialog;
 
     @ViewById(R.id.toolbarHeader)
     Toolbar mToolbar;
@@ -41,8 +70,6 @@ public class MainActivity extends BaseActivity {
 
     @ViewById(R.id.tvTitleHeader)
     TextView mTvTitleHeader;
-
-    private List<RowItem> mListRowItem = new ArrayList<>();
 
     private void loadDataRecycler() {
         final String[] listTitle = getResources().getStringArray(R.array.main_array_row_title);
@@ -76,7 +103,7 @@ public class MainActivity extends BaseActivity {
                 Toast.makeText(MainActivity.this, getString(R.string.main_menu_text_action_setting), Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.mnuActionUpload:
-                Toast.makeText(MainActivity.this, getString(R.string.main_menu_text_action_upload), Toast.LENGTH_SHORT).show();
+                showFileChooser();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -87,6 +114,101 @@ public class MainActivity extends BaseActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_upload_toolbar, menu);
         return true;
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType(getString(R.string.main_constant_video_type_all_file_mp4));
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, getString(R.string.main_constant_file_choices_text)),
+                    FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, getResources().getString(R.string.main_toast_text_error_content),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initProgress() {
+        mProgressDialog = new ProgressDialog(MainActivity.this);
+        mProgressDialog.setTitle(getString(R.string.main_progressdialog_text_title_progressdialog));
+        mProgressDialog.setMessage(getString(R.string.main_progressdialog_text_message_progressdialog));
+        mProgressDialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FILE_SELECT_CODE && resultCode == Activity.RESULT_OK) {
+            final String path = geRealPathFromUri(MainActivity.this, data.getData());
+            MediaPlayer mediaPlayer = MediaPlayer.create(this, Uri.parse(path));
+            File file = new File(path);
+            final String name = file.getName();
+            final String duration = String.valueOf(mediaPlayer.getDuration());
+            final String date = getDateSystem();
+            StorageReference storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(getString(R.string.main_firebase_linkbase_database_video_online));
+            StorageReference fileVideo = storageReference.child(getString(R.string.main_constant_video_type_video) + name);
+            UploadTask uploadTask = fileVideo.putFile(data.getData());
+            initProgress();
+            uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    mProgressDialog.setMessage(getString(R.string.main_progressdialog_text_message_progressdialog_loading) + (int) progress + " %");
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    mProgressDialog.dismiss();
+                    String url = taskSnapshot.getMetadata().getDownloadUrl().toString();
+                    VideoOnline videoOnline = new VideoOnline(url, name, getString(R.string.main_firebase_object_user_author) + UUID.randomUUID().toString(), date, duration);
+                    uploadVideoOnline(videoOnline);
+                }
+            }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onPaused(UploadTask.TaskSnapshot taskSnapshot) {
+                }
+            });
+        }
+    }
+
+    private void uploadVideoOnline(VideoOnline videoOnline) {
+        Firebase.setAndroidContext(this);
+        Firebase root = new Firebase(getString(R.string.main_firebase_root_database_video_online));
+        root.child(getString(R.string.main_firebase_object_user)).push().setValue(videoOnline);
+
+    }
+
+    public String geRealPathFromUri(Context context, Uri contentUri) {
+        String path = null;
+        if (contentUri.getScheme().equals(getString(R.string.main_constant_file_type_video_online))) {
+            path = contentUri.getPath();
+        } else if (contentUri.getScheme().equals(getString(R.string.main_constant_content_type_video_online))) {
+            Cursor cursor = context.getContentResolver().query(contentUri, null, null, null, null);
+            assert cursor != null;
+            cursor.moveToFirst();
+            String documentId = cursor.getString(0);
+            documentId = documentId.substring(documentId.lastIndexOf(":") + 1);
+            cursor.close();
+            cursor = context.getContentResolver().query(
+                    android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                    null, MediaStore.Video.Media._ID + " = ? ", new String[]{documentId}, null);
+            assert cursor != null;
+            if (cursor.getCount() == 0) {
+                path = getString(R.string.main_constant_file_path_text_error);
+            } else {
+                cursor.moveToFirst();
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+                cursor.close();
+            }
+        }
+        return path;
+    }
+
+    private String getDateSystem() {
+        DateFormat dateFormat = new SimpleDateFormat(getString(R.string.main_simple_date_format_system_date), Locale.ENGLISH);
+        return dateFormat.format(Calendar.getInstance().getTime());
     }
 
     @Override
@@ -110,7 +232,7 @@ public class MainActivity extends BaseActivity {
                 }
                 return false;
             }
-            
+
             @Override
             public void onTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
             }
@@ -122,9 +244,10 @@ public class MainActivity extends BaseActivity {
         layoutManager = new LinearLayoutManager(this);
         mRecycleViewMenu.setLayoutManager(layoutManager);
         actionBarDrawerToggle = getActionBarDrawerToggle(mToolbar);
-            mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
     }
+
 
     private GestureDetector getGestureDetector() {
         return new GestureDetector(MainActivity.this,
